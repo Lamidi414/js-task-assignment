7e54e2e00df532e15f70f442bad08e4d0fbeb06b
Lamidi414 <abdulhamidsalisu2005@gmail.com>
2025-11-24 00:56:10 -0800

    task completed

---
+++ variable-dataset/script.js
@@ -0,0 +1,15 @@
+let name, age, isStudentStatus, favouriteProgrammingLanguage;
+name = "Bello";
+age = 22;
+isStudentStatus = true;
+favouriteProgrammingLanguage = "python"
+console.log(name, age, isStudentStatus, favouriteProgrammingLanguage);
+
+//Modifying the data in more organzed way (using object and array)
+const person = {
+    name: "Bello",
+    age: 22,
+    isStudentStatus: true,
+    favouriteProgrammingLanguage: "python"  
+}
+console.log(person);
